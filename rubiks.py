dd6ee6864e24757a42cd7accbc373ecd11c9e894
Zeyad Yasser <zyadyasser626@gmail.com>
2021-06-19 00:23:29 +0200

    1. cube became a dictionary instead of an array with colors as keys and matricies as values 2. created variables to store the cube order 3.started in the R function

--- rubiks.py
+++ rubiks.py
@@ -1,13 +1,22 @@
 class RubiksCube:
     def __init__(self, n, front) -> None:
-        self.front = front
-        faces = "BRGYWO"
-        self.cube = [
-            [[color for i in range(n)]for j in range(n)]for color in faces
-        ]
-        
+        self.front = front   
+        self.n = n       
+        faces = "BRGYWO"            #colors of the cube
+        self.cube = {}              #dictionary to store the 6 faces of the cube
+        for color in faces:         #create and store a face for every color
+            self.cube[color] = [[color for i in range(n)]for j in range(n)]     #each face is represented by a 2d array
+        self.vertical_order = "BYGW"
+        self.horizontal_order = "BRGO"
+
+    def R(self, face, reverse = False):
+        idx = self.vertical_order.index(face)
+        tmp = [row[self.n - 1] for row in self.cube[face]]      #store the right most values in each row of the face
+        for i in range(4):
+            pass
+
     def print_cube(self):
-        for face in self.cube:
+        for face in self.cube.values():
             for row in face:
                 print(row)
             print("*"*40)
